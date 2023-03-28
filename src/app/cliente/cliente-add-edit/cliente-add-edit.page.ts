30a2b3240e470e01956ac0b504e16c82bd0f422a
PedroHSassii <pedro.2019010304@aluno.edu.iffar.br>
2023-03-28 19:28:35 -0300

    Validacao de Erros

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,6 +1,14 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
+
+
+// parei no slide 88 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
 @Component({
   selector: 'app-cliente-add-edit',
   templateUrl: './cliente-add-edit.page.html',
@@ -11,7 +19,33 @@ export class ClienteAddEditPage implements OnInit {
   cliente = {nascimento: null, renda: null, tel: null, email: null, nome: null};
    //@ViewChild('inputNome', {read:ElementRef}) nome!:ElementRef;
   clienteForm! : FormGroup;
+
+  hasErrors = false;
+  errorMessage: string[]|undefined;
+
   constructor(private formBuilder: FormBuilder) { }
+
+  validationMessages ={
+    nome: [
+      {type: 'required', message: 'Nome é obrigatório'},
+      {type: 'minlength', message: 'Nome deve ter ao menos 3 caracteres'},
+      {type: 'maxlength', message: 'Nome não deve ter ao mais 50 caracteres'}
+    ],
+    email: [
+      {type: 'required', message: 'E-mail é obrigatório'},
+      {type: 'email', message: 'O e-mail deve ter um formato válido'}
+    ],
+    tel: [
+      {type: 'required', message: 'Telefone é obrigatório'}
+    ],
+    renda: [
+      {type: 'required', message: 'Renda é obrigatório'},
+      {type: 'min', message: 'Renda deve ser maior que zero'}
+    ],
+    nascimento: [
+      {type: 'required', message: 'Nascimento é obrigatório'}
+    ],
+  }
 
   ngOnInit() {
     this.clienteForm = this.formBuilder.group({
@@ -29,6 +63,22 @@ export class ClienteAddEditPage implements OnInit {
   public nome: string | undefined;
 
   submit() {
-    if(this.clienteForm.invalid || this.clienteForm.pending) {return};
+  /*  this.errorMessage = [];
+    if(this.clienteForm.get('nome')!.hasError('required')) {
+      this.errorMessage.push('Nome é obrigatório');
+    }
+    if(this.clienteForm.get('email')!.hasError('required')) {
+      this.errorMessage.push('Email é obrigatório');
+    }
+    if(this.clienteForm.get('tel')!.hasError('required')) {
+      this.errorMessage.push('Telefone é obrigatório');
+    }
+    if(this.clienteForm.get('renda')!.hasError('required')) {
+      this.errorMessage.push('Renda é obrigatório');
+    }
+    if(this.clienteForm.get('nascimento')!.hasError('required')) {
+      this.errorMessage.push('Nascimento é obrigatório');
+    }
+    this.hasErrors = this.errorMessage.length > 0;*/
   }
 }
